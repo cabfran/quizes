d70b448b87401986708767636583ce4bfa2b6c28
Anat <trishin.anatoly2018@yandex.ru>
2020-01-06 13:36:00 +0300

    create active quiz

--- src/containers/quiz/Quiz.js
+++ src/containers/quiz/Quiz.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import classes from './Quiz.module.css'
-
+import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
 
 
 class Quiz extends Component {
@@ -8,13 +8,18 @@ class Quiz extends Component {
     state= {
         quiz:[]
     }
-    render () {
-return(
+
+render () {
+  return(
     <div className={classes.Quiz}>
-<h1>Quiz</h1>
+      
+      <div className={classes.QuizWrappper}>
+      <h1>Quiz</h1>
+          <ActiveQuiz />
+      </div>
     </div>
-)
-    }
+    )
+  }
 }
 
 
